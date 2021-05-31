4a6b357794d3052a1f9ceaab0a1b520a9e3f34b6
stan-kostadinov <strongholder@gmail.com>
2021-05-31 18:51:15 +0300

    changed routing in app

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -48,8 +48,8 @@ class Directory extends React.Component {
         return (
             <div className="directory-menu">
                 {
-                    this.state.sections.map( ({ title, imageUrl, id, size }) => (
-                        <MenuItem key={id} title={title} url={imageUrl} size={size} />
+                    this.state.sections.map( ({ title, imageUrl, id, size, linkUrl }) => (
+                        <MenuItem key={id} title={title} url={imageUrl} size={size} link={linkUrl} />
                     ))
                 }
             </div>
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import './menu-item.styles.scss';
+import { withRouter } from 'react-router-dom';
 
-const MenuItem = ({title, url, size}) => (
+const MenuItem = ({title, url, size, history, link, match}) => (
 
-            <div className={`${size} menu-item`}>
+            <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${link}`)}>
                 <div className="backgroundImage" style={{backgroundImage: `url(${url})`}} />
                 <div className="content">
                     <h1 className="title"> { title } </h1>
-                    <span className="subtitle"> Shop now</span>
+                    <span className="subtitle"> Shop now  </span>
                 </div>
             </div>
 
 )
 
-export default MenuItem;
+export default withRouter(MenuItem);
