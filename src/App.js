16b92f2ce825183a76078d5336980356e97f7bf0
stan-kostadinov <strongholder@gmail.com>
2021-06-04 12:32:44 +0300

    added header

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import { Route, Switch } from 'react-router';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shoppage/shop.component';
+import Header from './components/header/header.component';
 
 
 function App() {
   return (
-
+    <div>
+    <Header />
     <Switch>
       <Route exact path="/" component={ HomePage } />
       <Route path="/shop" component={ ShopPage } />
     </Switch>
-
+    </div>
   );
 }
 
