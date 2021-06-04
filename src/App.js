67645461d4b685090cf21548fef37ff3f1d40efe
stan-kostadinov <strongholder@gmail.com>
2021-06-04 15:22:37 +0300

    new sign-in page with custom components

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shoppage/shop.component';
 import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
     <Switch>
       <Route exact path="/" component={ HomePage } />
       <Route path="/shop" component={ ShopPage } />
+      <Route path="/signin" component={ SignInAndSignUpPage } />
     </Switch>
     </div>
   );
