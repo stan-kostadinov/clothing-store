ff793175182773c6d41fbc0e47ec6bb299beb689
stan-kostadinov <strongholder@gmail.com>
2021-06-11 13:37:32 +0300

    changes to redux func

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import './header.styles.scss';
 import { auth } from '../../firebase/firebase.utils';
+import { connect } from 'react-redux';
 
 const Header = ({ currentUser }) => (
 
@@ -26,4 +27,8 @@ const Header = ({ currentUser }) => (
 
 )
 
-export default Header;
+const mapStateToProps = state => ({
+    currentUser: state.user.currentUser
+})
+
+export default connect (mapStateToProps) (Header);
