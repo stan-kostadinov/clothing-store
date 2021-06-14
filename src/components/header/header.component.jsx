34460833af87afad27da4d5a4c91da2b2f9d66ec
stan-kostadinov <strongholder@gmail.com>
2021-06-14 18:36:03 +0300

    added cart show/hide with redux state to hide it

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,8 +4,10 @@ import { ReactComponent as Logo } from '../../assets/crown.svg';
 import './header.styles.scss';
 import { auth } from '../../firebase/firebase.utils';
 import { connect } from 'react-redux';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
 
     <div className="header">
         <Link className="logo-container" to="/">
@@ -22,13 +24,17 @@ const Header = ({ currentUser }) => (
             <div className="option" onClick={() => auth.signOut()}>SIGN OUT</div>
             : <Link className="option" to="/signin"> SIGN IN</Link>
             }
+
+            <CartIcon onClick={ () => document.getElementsByClassName('cart-dropdown').detach() } />
         </div>
+        { hidden ? null : <CartDropdown /> }
     </div>
 
 )
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+    currentUser,
+    hidden
 })
 
 export default connect (mapStateToProps) (Header);
