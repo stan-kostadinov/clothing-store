c3165655c47df94d387f9b64433cdf9c2a847bab
stan-kostadinov <strongholder@gmail.com>
2021-06-14 15:55:19 +0300

    did something with redux, haven't figured out why it works yet

--- src/App.js
+++ src/App.js
@@ -6,36 +6,32 @@ import ShopPage from './pages/shoppage/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import {auth, createUserProfileDocument} from './firebase/firebase.utils';
+import { setCurrentUser } from './redux/user/user.actions';
+import { connect } from 'react-redux';
 
 
 class App extends React.Component {
-constructor(){
-  super();
 
-  this.state = {
-    currentUser: null
-  }
-}
 
 unsubscribeFromAuth = null;
 
 componentDidMount () {
+  const { setCurrentUser } = this.props;
+
   auth.onAuthStateChanged( async userAuth => {
     if (userAuth) {
       const userRef = await createUserProfileDocument(userAuth);
 
 
       userRef.onSnapshot( snapShot => {
-        this.setState({
-          currentUser: {
+        setCurrentUser({
             id: snapShot.id,
             ...snapShot.data()
-          }
-        })
+        });
       });
       
     } else {
-      this.setState({currentUser: userAuth});
+      setCurrentUser({userAuth});
     }
   } )
 }
@@ -59,4 +55,8 @@ componentWillUnmount() {
   }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch( setCurrentUser(user) )
+});
+
+export default connect (null, mapDispatchToProps ) (App);
