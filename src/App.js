7aa8d5534fc8cd40e66599392168105b5e32ee98
stan-kostadinov <strongholder@gmail.com>
2021-06-14 16:30:42 +0300

    small changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch } from 'react-router';
+import { Route, Switch, Redirect } from 'react-router';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shoppage/shop.component';
@@ -48,12 +48,16 @@ componentWillUnmount() {
     <Switch>
       <Route exact path="/" component={ HomePage } />
       <Route path="/shop" component={ ShopPage } />
-      <Route path="/signin" component={ SignInAndSignUpPage } />
+      <Route exact path="/signin" render={() => this.props.currentUser ? (<Redirect to="/" />) : (<SignInAndSignUpPage />) } />
     </Switch>
     </div>
     )
   }
 }
+
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+})
 
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch( setCurrentUser(user) )
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -18,7 +18,7 @@ const Header = ({ currentUser }) => (
             <Link className="option" to="/contact">
                     CONTACT
             </Link>
-            {currentUser ?
+            { currentUser ?
             <div className="option" onClick={() => auth.signOut()}>SIGN OUT</div>
             : <Link className="option" to="/signin"> SIGN IN</Link>
             }
