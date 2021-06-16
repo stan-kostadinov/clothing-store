742d70b21fb9bd643fd6bf32af00b07e9931a5fe
stan-kostadinov <strongholder@gmail.com>
2021-06-16 12:43:29 +0300

    added cart functionality with react/redux

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import './collection-item.styles.scss';
+import CustomButton from '../custom-button/custom-button.component';
+import { connect } from 'react-redux';
+import { addItem } from '../../redux/cart/cart.actions';
 
-const CollectionItem = ({id, name, price, imageUrl}) => (
+const CollectionItem = ({item, addItem}) => {
+    const { name, price, imageUrl } = item;
+    return(
     <div className="collection-item">
         <div className="image" style={{backgroundImage: `url(${imageUrl})`}}></div>
         <div className="collection-footer">
             <span className="name">{ name }</span>
             <span className="price">{ price }</span>
         </div>
-    </div>
-)
 
-export default CollectionItem;
+        <CustomButton onClick={() => addItem(item)} inverted> ADD TO CART </CustomButton>
+
+    </div>
+)}
+
+const mapDispatchToProps = dispatch => ({
+    addItem: item => dispatch(addItem(item))
+})
+
+export default connect (null, mapDispatchToProps) (CollectionItem);
--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import './custom-button.styles.scss';
 
-const CustomButton = ({ children, isGoogleSignIn, ...otherprops }) => (
-    <button className={`${isGoogleSignIn ? `google-signin`:''} custom-button`} {...otherprops} >
+const CustomButton = ({ children, isGoogleSignIn, inverted, ...otherprops }) => (
+    <button className={`${isGoogleSignIn ? `google-signin`:''} custom-button ${inverted ? `inverted`:''} custom-button`} {...otherprops} >
         { children }
     </button>
 )
